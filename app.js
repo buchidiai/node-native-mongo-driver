e3dbffc8194394a595c6fbc71ab498c5803d23b9
54 Bridge <54bridgeapp@gmail.com>
2020-01-07 11:01:36 -0600

    simplify with mongoose

--- app.js
+++ app.js
@@ -1,157 +1,31 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+const mongoose = require("mongoose");
 
-// Connection URL
-const url = "mongodb://localhost:27017";
-
-// Database Name
-const dbName = "TeamsDB";
-
-// Create a new MongoClient
-const client = new MongoClient(url, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
+mongoose.connect(`mongodb://localhost:27017/Business`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      client.close();
-    });
-  });
+const businessSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  zipCode: Number,
+  open: Boolean,
+  socialMedia: Array,
+  coordinates: {
+    type: [Number],
+    default: [0, 0]
+  }
 });
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection("docs");
-  // Insert some documents
-  collection.insertMany(
-    [
-      {
-        key: "chelsea",
-        name: "Chelsea FC",
-        code: "CHE"
-      },
-      {
-        key: "arsenal",
-        name: "Arsenal FC",
-        code: "ARS"
-      },
-      {
-        key: "tottenham",
-        name: "Tottenham Hotspur FC",
-        code: "TOT"
-      },
-      {
-        key: "westham",
-        name: "West Ham United FC",
-        code: "WHU"
-      },
-      {
-        key: "crystalpalace",
-        name: "Crystal Palace FC",
-        code: "CRY"
-      },
-      {
-        key: "manutd",
-        name: "Manchester United FC",
-        code: "MUN"
-      },
-      {
-        key: "mancity",
-        name: "Manchester City FC",
-        code: "MCI"
-      },
-      {
-        key: "everton",
-        name: "Everton FC",
-        code: "EVE"
-      },
-      {
-        key: "liverpool",
-        name: "Liverpool FC",
-        code: "LIV"
-      },
-      {
-        key: "newcastle",
-        name: "Newcastle United FC",
-        code: "NEW"
-      },
-      {
-        key: "astonvilla",
-        name: "Aston Villa FC",
-        code: "AVL"
-      },
-      {
-        key: "southampton",
-        name: "Southampton FC",
-        code: "SOU"
-      },
-      {
-        key: "leicester",
-        name: "Leicester City FC",
-        code: "LEI"
-      },
-      {
-        key: "bournemouth",
-        name: "AFC Bournemouth",
-        code: "BOU"
-      },
-      {
-        key: "norwich",
-        name: "Norwich City FC",
-        code: "NOR"
-      },
-      {
-        key: "watford",
-        name: "Watford FC",
-        code: "WAT"
-      },
-      {
-        key: "burnley",
-        name: "Burnley FC",
-        code: "BUR"
-      },
-      {
-        key: "brightonhovealbionfc",
-        name: "Brighton & Hove Albion FC",
-        code: null
-      },
-      {
-        key: "sheffieldunitedfc",
-        name: "Sheffield United FC",
-        code: null
-      },
-      {
-        key: "wolverhamptonwanderersfc",
-        name: "Wolverhampton Wanderers FC",
-        code: null
-      }
-    ],
-    function(err, result) {
-      assert.equal(err, null);
-      assert.equal(20, result.result.n);
-      assert.equal(20, result.ops.length);
-      console.log("Inserted 20 documents into the collection");
-      callback(result);
-    }
-  );
-};
+const Business = mongoose.model("Business", businessSchema);
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection("docs");
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits);
-  });
-};
+const business = new Business({
+  name: `Angie's Food Trunk`,
+  address: `500 dallas dr, dallas, tx`,
+  zipCode: 75501,
+  open: true,
+  socialMedia: ["angies_trunk"],
+  coordinates: [40.73061, -73.935242]
+});
+
+business.save().then(() => console.log("saved"));
